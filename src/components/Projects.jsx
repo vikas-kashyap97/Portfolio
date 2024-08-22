471224b8ae48f5b585461cffb03e75788c1ef75b
Vikas Kashyap <159269579+princeessjay@users.noreply.github.com>
2024-08-22 17:39:37 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,81 +17,76 @@ const Projects = () => {
         <h1 className="text-xl md:text-2xl lg:text-3xl font-bold pl-2">
           PROJECTS
         </h1>
-
         <div className="w-full md:w-4/5 mx-auto mt-10 flex flex-col md:flex-row justify-between items-center gap-10">
-
           {/* project first  */}
           <div className=" md:w-1/2 rounded-lg p-5 shadow-lg hover:scale-105 transition-transform">
-            <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">Modern Blog Website</h1>
+            <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">
+              Hospital Management System (Frontend)
+            </h1>
             <img
-              src="images/Capture.JPG"
+              src="images/Frontend dashboard.jpg"
               alt="project-img"
               className="h-60 w-full rounded-lg"
             />
 
             <div className="flex gap-10 mt-5 justify-starts">
               <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://modern-blog-app.vercel.app/">Live</a>
+                <a href="https://hospital-management-system-cwv.netlify.app/">
+                  Live
+                </a>
               </Button>
               <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://github.com/princeessjay/Modern_Blog_App.git">Github</a>
+                <a href="https://github.com/princeessjay/HMS_FRONTEND.git">
+                  Github
+                </a>
               </Button>
             </div>
           </div>
 
           {/* project second */}
           <div className=" md:w-1/2 rounded-lg p-5 shadow-lg  hover:scale-105 transition-transform">
-          <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">OpenAI-App</h1>
+            <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">
+              Hospital Management System (Admin)
+            </h1>
             <img
-              src="images\OpenAI.avif"
+              src="images/Admin dashboard.jpg"
               alt="project-img"
               className="h-60 w-full rounded-lg"
             />
 
             <div className="flex gap-10 mt-5 justify-start">
               <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://openaiapp.netlify.app/">Live</a>
+                <a href="https://admin-dashboard-hms-cwv.netlify.app/login">
+                  Live
+                </a>
               </Button>
               <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://github.com/princeessjay/OpenAI-app.git">Github</a>
+                <a href="https://github.com/princeessjay/HMS_DASHBOARD.git">
+                  Github
+                </a>
               </Button>
             </div>
           </div>
 
           {/* project third */}
           <div className=" md:w-1/2 rounded-lg p-5 shadow-lg  hover:scale-105 transition-transform">
-          <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">Resume App</h1>
+            <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">
+              Real-Time Dashboard
+            </h1>
             <img
-              src="images\Resume.avif"
+              src="images/Dashboard.jpg"
               alt="project-img"
               className="h-60 w-full rounded-lg"
             />
 
             <div className="flex gap-10 mt-5 justify-start">
               <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://resumeapp0.netlify.app/">Live</a>
+                <a href="https://dashboard-beryl-six.vercel.app/">Live</a>
               </Button>
               <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://github.com/princeessjay/Resume-app.git">Github</a>
-              </Button>
-            </div>
-          </div>
-
-          {/* project fourth */}
-          <div className=" md:w-1/2 rounded-lg p-5 shadow-lg  hover:scale-105 transition-transform">
-          <h1 className="text-sm md:text-lg lg:text-xl mb-2 ">Quiz Game</h1>
-            <img
-              src="images/Quiz App.png"
-              alt="project-img"
-              className="h-60 w-full rounded-lg"
-            />
-
-            <div className="flex gap-10 mt-5 justify-start">
-              <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://quiz-app-gules-theta.vercel.app/">Live</a>
-              </Button>
-              <Button className="px-6 lg:px-8 py-3 lg:py-4  rounded-md bg-gradient-to-r from-blue-500 to-indigo-900 hover:scale-105">
-                <a href="https://github.com/princeessjay/Quiz-App.git">Github</a>
+                <a href="https://github.com/princeessjay/Dashboard.git">
+                  Github
+                </a>
               </Button>
             </div>
           </div>
